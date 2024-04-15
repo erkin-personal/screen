8c55f50f369018647d0d2df5de5727935fc24df2
h erkin atak <h.erkin.atak@outlook.com>
2024-04-16 01:00:50 +0300

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from pyvirtualdisplay import Display
 
-# Setting up a virtual display to run headless Firefox
+# Setting up a virtual display to run headless Chrome
 display = Display(visible=0, size=(1024, 768))
 display.start()
 
-# Setting up Firefox options
+# Setting up Chrome options
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')  # Bypass OS security model, discouraged
 options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
+options.add_argument('--disable-gpu')  # GPU hardware acceleration isn't necessary for headless viewing
+options.add_argument('--window-size=1920x1080')  # Define window size for screenshots or elements rendering
 
 # Setting up WebDriver
-service = FirefoxService(executable_path=GeckoDriverManager().install())
-driver = webdriver.Firefox(service=service, options=options)
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
 # Accessing the webpage
 url = "https://sunrise.net"  # Hardcoded URL
