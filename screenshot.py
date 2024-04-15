1194cda1e4853ee11efaa479fa14f7282c291eee
h erkin atak <h.erkin.atak@outlook.com>
2024-04-16 02:07:34 +0300

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from pyvirtualdisplay import Display
 
 # Setting up a virtual display to run headless Chrome
@@ -16,8 +15,8 @@ options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource pro
 options.add_argument('--disable-gpu')  # GPU hardware acceleration isn't necessary for headless viewing
 options.add_argument('--window-size=1920x1080')  # Define window size for screenshots or elements rendering
 
-# Setting up WebDriver
-service = ChromeService(executable_path=ChromeDriverManager().install())
+# Assuming ChromeDriver is in the system's PATH, specify it directly without using ChromeDriverManager
+service = ChromeService(executable_path='/usr/local/bin/chromedriver-linux64/chromedriver')
 driver = webdriver.Chrome(service=service, options=options)
 
 # Accessing the webpage
