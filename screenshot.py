4e0901e230346a597bbd59dbec64248ee34e9aed
h erkin atak <h.erkin.atak@outlook.com>
2024-04-15 20:12:29 +0300

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,18 +1,32 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 from pyvirtualdisplay import Display
-import os
 
+# Setting up a virtual display to run headless Firefox
 display = Display(visible=0, size=(1024, 768))
 display.start()
 
-options = webdriver.FirefoxOptions()
+# Setting up Firefox options
+options = Options()
 options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--no-sandbox')  # Bypass OS security model, discouraged
+options.add_argument('--disable-dev-shm-usage')  # Overcome limited resource problems
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-driver.get(os.getenv('url'))
+# Setting up WebDriver
+service = FirefoxService(executable_path=GeckoDriverManager().install())
+driver = webdriver.Firefox(service=service, options=options)
+
+# Accessing the webpage
+url = "https://sunrise.net"  # Hardcoded URL
+driver.get(url)
+
+# Taking screenshot
 driver.save_screenshot('/screen/screen.png')
+
+# Closing the browser
 driver.quit()
+
+# Stop the display
+display.stop()
